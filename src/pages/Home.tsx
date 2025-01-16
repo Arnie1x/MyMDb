b138ee6d1d9eb0c0e203307263bf5f8556f65c03
Arnie1x <arnie752@gmail.com>
2025-01-16 15:50:33 +0300

    feat(pages/Home.tex): Created Search Function that returns the searched term to the user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,35 @@
+import React from "react";
 import MovieCard from "../assets/components/MovieCard";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = React.useState("");
+
     const movies: Movie[] = [
         {
             id: "0",
             Poster: "",
             Title: "Five Nights at Freddy's",
             Year: "2024"
-        }
+        },
+        {
+            id: "1",
+            Poster: "",
+            Title: "The Matrix",
+            Year: "1999"
+        },
     ]
+
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        alert(`You searched for ${searchQuery}`);
+    }
     return (
         <div className="home">
-            <h1 className="text-5xl">Home</h1>
+            {/* <h1 className="text-5xl">Home</h1> */}
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for a Movie..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                <button type="submit">Search</button>
+            </form>
             <div className="movie-grid">
                 {movies.map((movie) => (
                     <MovieCard key={movie.id} movie={movie} />
